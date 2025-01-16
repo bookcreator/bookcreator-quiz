6220a09777ad2ba70617e17f9cadb13a00c673c3
Dan Porter <dp@bookcreator.com>
2025-01-16 09:32:25 +0000

    Updated test

--- __tests__/app/api/submit.test.js
+++ __tests__/app/api/submit.test.js
@@ -13,7 +13,20 @@ it("should return data with status 200", async () => {
   const response = await POST(requestObj);
   const body = await response.json();
   expect(response.status).toBe(200);
-  expect(body).toEqual({
-    answers: [{ correct: false }, { correct: false }, { correct: true }],
-  });
+  expect(Object.keys(body)).toEqual([
+    "answers",
+    "userScore",
+    "skillScore",
+    "totalScore",
+    "leaderboard",
+    "userName",
+  ]);
+  expect(body.userScore).toEqual(1);
+  expect(body.skillScore).toBeGreaterThan(-1);
+  expect(body.skillScore).toBeLessThan(3001);
+  expect(body.userName).toEqual("Player 1");
+  expect(body.leaderboard.length).toEqual(10001);
+  const userEntry = body.leaderboard.find(user => user.name === "Player 1");
+  expect(userEntry.score).toEqual(body.totalScore);
+  expect(userEntry.name).toEqual(body.userName);
 });
